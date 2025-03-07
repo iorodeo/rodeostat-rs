c8189890066059c5e509f420bfa703c39dac0e9f
Will Dickson <will@iorodeo.com>
2025-03-07 10:11:14 -0800

    Worked on deserialization of test parameters.

--- src/param.rs
+++ src/param.rs
@@ -32,3 +32,34 @@ pub struct ConstantParam {
     pub value: f32, 
     pub duration: f32,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SquareWaveParam {
+    pub quiet_value: f32, 
+    pub quiet_time: f32, 
+    pub start_value: f32,
+    pub final_value: f32,
+    pub step_value: f32,
+    pub amplitude: f32, 
+    pub window: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LinearSweepParam {
+    pub quiet_value: f32, 
+    pub quiet_time: f32, 
+    pub start_value: f32,
+    pub final_value: f32,
+    pub duration: u32,
+}
+
+//#[derive(Serialize, Deserialize, Debug)]
+//#[serde(rename_all = "camelCase")]
+//pub struct Chronoamp {
+//    //{'quietValue': 0, 'quietTime': 0, 'step': [[1000, 0.5], [1000, -0.5]]}
+//    pub quiet_value: f32, 
+//    pub quiet_time: f32, 
+//    pub step: Vec<Vec<f32>>,
+//}
